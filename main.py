2c55c1d6127e2fb555b01d8fbbd76a80e4f0a70f
Javi <javierplazasisques@gmail.com>
2021-05-17 13:20:37 +0200

    Modelo

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from tensorflow.keras.layers import *
 from numpy.lib.type_check import imag
 from numpy import random
 
-
 import matplotlib.pyplot as plt
+import tensorflow.keras as kr
 import tensorflow as tf
 import numpy as np
 import os
+
+from tensorflow.python.keras import initializers
+from tensorflow.python.ops.gen_array_ops import shape
 
 INPATH = './inputImages/'           #Ruta de las imagenes de entrada
 OUPATH = './targetImages/'          #Ruta de las imagenes de salida
@@ -28,6 +31,13 @@ def main():
     train_dataset = generate_train_dataset(tr_urls)
     test_dataset = generate_test_dataset(ts_urls)
     #show_dataset_images(test_dataset)
+
+    #Modelo
+    model = generator()
+
+    gen_output = model(((train_dataset[0] + 1) * 255), training=False)
+    plt.imshow(gen_output[0,...])
+    plt.show()
 
 
 
@@ -130,5 +140,112 @@ def show_dataset_images(dataset):
     for inimg, tgimg in dataset.take(5):
         show_unnormalized_image(tgimg[0, ...]) # le quitamos la primera dimension que hace referencia al batch
         show_unnormalized_image(inimg[0, ...])
+
+def downsample(filters, apply_batchnorm = True):
+    result = kr.Sequential()
+
+    initializer = tf.random_normal_initializer(0, 0.02)     #Ruido Gaussiano y derivada
+
+    #Capa convolucional
+    result.add(Conv2D(filters, 
+    kernel_initializer=initializer, 
+    kernel_size=4, 
+    strides=2, #Para dividir el tamaño a la mitad
+    padding='same',
+    use_bias=not apply_batchnorm
+    ))
+
+    if apply_batchnorm:
+        #Capa BatchNorm
+        result.add(BatchNormalization())
+
+    #Capa de activacion
+    result.add(LeakyReLU())
+
+
+    return result
+
+def upsample(filters, apply_dropout = False):
+    result = kr.Sequential()
+
+    initializer = tf.random_normal_initializer(0, 0.02)     #Ruido Gaussiano y derivada
+
+    #Capa convolucional inversa
+    result.add(Conv2DTranspose(filters, 
+    kernel_initializer=initializer, 
+    kernel_size=4, 
+    strides=2, 
+    padding='same',
+    use_bias=False
+    ))
+
+     #Capa BatchNorm
+    result.add(BatchNormalization()) 
+
+    if apply_dropout:
+       result.add(Dropout(0.5))
+
+    #Capa de activacion
+    result.add(ReLU())
+
+
+    return result
+
+def generator():
+
+    inputs = tf.keras.layers.Input(shape=[None, None, 3]) #Para que nuestra imagen pueda tener diferenetes tamaños
+
+    down_stack = [
+        downsample(64, apply_batchnorm=False),      #(bs,   128,    128,    64)
+        downsample(128),                            #(bs,   64,     64,     128)
+        downsample(256),                            #(bs,   32,     32,     256)
+        downsample(512),                            #(bs,   16,     16,     512)
+        downsample(512),                            #(bs,   8,      8,      512)
+        downsample(512),                            #(bs,   4,      4,      512)
+        downsample(512),                            #(bs,   2,      2,      512)
+        downsample(512),                            #(bs,   1,      1,      512)
+    ]
+
+    up_stack = [
+        upsample(512, apply_dropout=True),          #(bs,   2,      2,      1024)
+        upsample(512, apply_dropout=True),          #(bs,   4,      4,      1024)
+        upsample(512, apply_dropout=True),          #(bs,   8,      8,      1024)
+        upsample(512),                              #(bs,   16,     16,     1024)
+        upsample(256),                              #(bs,   32,     32,     512)
+        upsample(128),                              #(bs,   64,     64,     256)
+        upsample(64),                               #(bs,   128,    128,    128)
+    ]
+
+    initializer = tf.random_normal_initializer(0, 0.02)     #Ruido Gaussiano y derivada
+
+    last = Conv2DTranspose(
+        filters=3, #Ya que queremos una imagen RGB
+        kernel_size=4,
+        strides=2, #Para aumentar el doble el tamaño
+        padding='same',
+        kernel_initializer=initializer,
+        activation='tanh' #Ya que queremos una salida de -1 a 1
+    )
+
+    #Conectamos todas las capas del codificador
+    x = inputs
+    s = []      #Skip conncetions
+
+    concat = Concatenate()
+
+    for down in down_stack:
+        x = down(x)
+        s.append(x)
+    
+    #Conectamos todas las capas del decodificador
+    s = reversed(s[:-1]) #El ultimo elemento no nos hace falta
+    for up, sk in zip(up_stack, s):
+        x = up(x)
+        x = concat([x, sk])
+    
+    last = last(x)
+    model = kr.Model(inputs=inputs, outputs=last)
+    model.summary()
+    return model
 
 main()
